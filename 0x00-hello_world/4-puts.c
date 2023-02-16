91f5a2254e107590e982451bd690aca8973b14a5
Tewaa <adat2013@yahoo.com>
2023-02-16 12:51:01 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdioh>
+
+/**
+ * main - A program that prints a line with putts function
+ * Return:0 (success)
+ */
+
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
