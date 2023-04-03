2d396176f580b5eb06e4c2f88e52ddf0b46bad1f
Tewaa <adat2013@yahoo.com>
2023-04-03 16:54:27 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *prints_listint - prints all singly list elements
+ *@n-prints nodes
+ *
+ *Return:the number of nodes
+*/
+size_t print_listint(const listint_t *h);
+{
+	size_t num = 0;
+
+while (h != null)
+{
+printf("%d\n", h->n);
+num++;
+h = h->next;
+}
+return (num);
+}
+
